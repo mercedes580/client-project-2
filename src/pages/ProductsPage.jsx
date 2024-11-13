946e0e486082458f6987bfd66663f72215ace6d3
Pedro Rosique <rosiquemurciapedro@gmail.com>
2024-11-13 14:53:10 +0100

    first form created

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,13 +1,18 @@
+import { Link } from "react-router-dom"
 import BakeryList from "../components/BakeryList/BakeryList"
 
 
 const ProductsPage = () => {
 
     return (
-        <div className="BakeryCard">
+        <div className="Products">
             <BakeryList />
 
+            <Link to={"/productos/añadir"}><button className="CreateButton">
+                <i className="fas fa-plus icon-margin"></i>&nbsp;
+                Crear nuevo producto</button></Link>
         </div>
+
     )
 }
 
--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,8 +1,9 @@
 import { Routes, Route } from 'react-router-dom'
 import HomePage from '../pages/HomePage'
 import ProductsPage from '../pages/ProductsPage'
-import AboutPage from '../pages/AboutPage'
-import ContactPage from '../pages/ContactPage'
+import AboutPage from '../pages/AboutPage/AboutPage'
+import ContactPage from '../pages/ContactPage/ContactPage'
+import AddNewProductPage from '../pages/AddProductPage/AddProductPage'
 
 const AppRoutes = () => {
 
@@ -14,7 +15,7 @@ const AppRoutes = () => {
                 <Route path='/productos/:id' element={<h1>detalle producto</h1>} />
                 <Route path='/sobre-nosotros' element={<AboutPage />} />
                 <Route path='/contacto' element={<ContactPage />} />
-                <Route path='/productos/añadir' element={<h1>producto a añadir</h1>} />
+                <Route path='/productos/añadir' element={<AddNewProductPage />} />
                 <Route path='/productos/editar/:id' element={<h1>editar producto</h1>} />
 
                 <Route path='*' element={<h1>404</h1>} />
