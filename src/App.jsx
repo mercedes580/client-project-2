8b7df049af8ff68751ba0f12cdb721004b93a7f9
Mercedes <mrcdssebastian@gmail.com>
2024-11-13 09:39:58 +0100

    3some

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,16 @@ import '../assets/css/owl.theme.default.min.css'
 import '../assets/css/themability_megamenu.css'
 import Navbar from './components/Navbar'
 import Header from './components/Header'
+
 import Footer from './components/Footer'
+import AppRoutes from './routes/AppRoutes'
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Header />
+      <AppRoutes />
       <Footer />
     </div>
   )
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,27 @@
 const HomePage = () => {
-
     return (
+        <div className="index page">
+            <header>
+                <h1>MPD BAKERY SHOP</h1>
+                <h2>Si te gusta uno... imagínate dos</h2>
+            </header>
+            <section className="welcome">
+                <h3>¿Tienen una boda? ¿Un cumpleaños? ¿O simplemente les apetecía darse un capricho?</h3>
+                <p>Permíteme decirte que has llegado al sitio y lugar adecuados donde los sueños son de chocolate y todo es posible.</p>
+            </section>
+            <section className="best listed products">
+                <h3>Nuestras Especialidades</h3>
+                <ul>
+                    <li>Brownies de suchard</li>
+                    <li>Cupcakes de fresa</li>
+                    <li>Tarta de zanahoria</li>
+                </ul>
+            </section>
+            <section className="about-us">
+                <h3>Sobre Nosotros</h3>
+                <p>En MPD BAKERY SHOP, te encantara probar los dulces con la mejor calidad posible y no solament eso sino con los mejores productos </p>
+            </section> </div>
+    );
+};
 
-        <h1>hola</h1>
-    )
-}
-
-export default HomePage
+export default HomePage;
